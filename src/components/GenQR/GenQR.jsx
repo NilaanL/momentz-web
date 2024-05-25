97714615a231e0bdef635a5173a4231b83e8af01
thareejanp <thareejan.pakkiyarajah@directfn.com>
2024-05-25 18:23:47 +0530

    download QR feature added

--- src/components/GenQR/GenQR.jsx
+++ src/components/GenQR/GenQR.jsx
@@ -22,6 +22,19 @@ const GenQR = () => {
     generateQrCode();
   }, [qrData, qrSize]); // The effect will run when qrData or qrSize changes
 
+  function downloadQR() {
+    fetch(qrImg)
+      .then((response) => response.blob())
+      .then((blob) => {
+        const link = document.createElement("a");
+        link.href = URL.createObjectURL(blob);
+        link.setAttribute("download", `qrcode-${qrData}.png`);
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      });
+  }
+
   return (
     <div className="app-container">
       <h1>QR Code Generator</h1>
@@ -44,13 +57,13 @@ const GenQR = () => {
         <button className="clear-button" onClick={() => setQrData("")}>
           Clear
         </button>
-        <button className="download-button">Download QR Code</button>
         <input
           type="number"
           value={qrSize}
           onChange={(e) => setQrSize(e.target.value)}
         />
         <button onClick={() => setQrSize("")}>Clear</button>
+        <button className="download-button" onClick={downloadQR}>Download QR</button>
       </div>
     </div>
   );
