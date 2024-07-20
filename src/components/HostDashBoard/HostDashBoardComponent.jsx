e6ce61791ed21c0390e4ccc486e8192ed42dbaa0
tha-ree-jan <thareejap.20@uom.lk>
2024-07-20 06:53:03 +0530

    deleted unwanted files and added navbar

--- src/components/HostDashBoard/HostDashBoardComponent.jsx
+++ src/components/HostDashBoard/HostDashBoardComponent.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import HostDashboard from './HostDashboard';
+import ResponsiveAppBar from '../../UI/NavBar';
 const HostDashBoardRoute = () => {
     return ( 
         <React.Fragment>
+        <ResponsiveAppBar/>
         <HostDashboard/>
         </React.Fragment>
      );
--- src/components/eventDashBoard/GuestDashboard.jsx
+++ src/components/eventDashBoard/GuestDashboard.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import MainComponent from './MainComponent';
+import ResponsiveAppBar from '../../UI/NavBar';
 const GuestDashBoard = () => {
     return ( <React.Fragment>
+    <ResponsiveAppBar/>
         <MainComponent/>
     </React.Fragment> );
 }
